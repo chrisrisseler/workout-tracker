baae6b837f51d7c425c70beae02605b01fb5b4c7
Chris Risseler <chrisrisseler43@gmail.com>
2021-04-05 22:21:27 -0400

    updated server to work with heroku

--- server.js
+++ server.js
@@ -15,7 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
